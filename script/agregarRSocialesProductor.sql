a59e18d0475246e1ee8228a6759c6d83a5f77758
lorenzo <wdq>
2020-07-20 21:11:37 -0300

    cambios en bd -> agrega tabla redocial_productor

---
+++ script/agregarRSocialesProductor.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `circuito_agroturistico`.`redsocial_Productor` (
+  `idRedsocial_Productor` INT NOT NULL AUTO_INCREMENT,
+  `idRed_social` INT NOT NULL,
+  `idProductor` INT NOT NULL,
+  PRIMARY KEY (`idRedsocial_Productor`));
