1abe83b7bfc7f82acf7d2e9ba88b1eae3ef48bde
lorenzopemac@gmail.com <lorenzopemac@gmail.com>
2020-07-29 16:26:46 -0300

    prueba imagen en catalogo

--- views/catalogo/index.php
+++ views/catalogo/index.php
@@ -1,9 +1,12 @@
 <?php
-
+use Yii;
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
+use yii\helpers\Url;
 use kartik\select2\Select2;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\ProductorSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -23,19 +26,14 @@ $this->params['breadcrumbs'][] = $this->title;
             'nombre',
             'descripcion',
             [
-
                 'attribute' => 'imagenes',
-    
                 'format' => 'html',
-    
                 'label' => 'Imagen',
-    
                 'value' => function ($data) {
-                    
-                    return Html::img('@web/uploads/'.'3.jpg',['width' => '60px']);
-    
+                    //Html::a("<img src='".Url::to(['uploads/'.'2.jpg'])."' class='user-image' />");
+                    return Html::a('<img src='.Yii::$app->basePath. '/uploads/2.jpg" style= "border-radius:50%;height: 90px; width: 90px;" class="img-circle" alt="User Image"/>' );
+                    //return Html::a('<img src="https://image.freepik.com/vector-gratis/perfil-empresario-dibujos-animados_18591-58479.jpg" style= "border-radius:50%;height: 60px; width: 60px;" class="img-circle" alt="User Image"/>' );
                 },
-    
             ],
             ['attribute'=>'productor',
              'value'=> 'productor.nombre'],
