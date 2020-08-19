a8d5330208f9efeb85a877867b9d7ec5407aaaba
lorenopemac <lorenzopemac@gmail.com>
2020-08-19 15:20:40 -0300

    Carousel en imagenes

--- views/producto/view.php
+++ views/producto/view.php
@@ -16,12 +16,42 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
     <div class="col-md-12 col-xs-12" style="padding-bottom: 25px;">     
         <div class="col-md-6 col-xs-12">     
-            <?php
-                foreach ($model->imagenes as $imagen)
-                {
-                    echo $imagen;
-                }
-            ?>
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                    <?php
+                        $indice =0;
+                        foreach ($model->imagenes as $imagen)
+                        {
+                            if($indice==0){
+                                $options = ['class' => ['item active']];
+                            }else{
+                                $options = ['class' => ['item']];
+                            }
+                            echo Html::tag('div', $imagen , $options);
+                            $indice=$indice +1;
+                        }
+                    ?>
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+            
         </div>
         <div class="col-md-6 col-xs-12">     
             <?= DetailView::widget([
