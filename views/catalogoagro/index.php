68b56ff5f02ae5dc6f1513f1029c6d85779da4d8
lorenopemac <lorenzopemac@gmail.com>
2020-11-11 20:28:56 -0300

    Faltante fix titulo en evento filtro

--- views/catalogoagro/index.php
+++ views/catalogoagro/index.php
@@ -155,7 +155,7 @@ $('.categoria').click(function(e){
                 var tamaño = Object.keys(res.productos).length;
                 for (var indice = 0; indice < tamaño; indice++) {
                 $( '.productos' ).append('<div class=\'col-lg-4 card \'><p style=text-align:center><img class=\'file-preview-image\' src='+direccion+'/aplicacion/CircuitoAgroturistico/web/uploads/'+ res.imagenes[indice] \n
-                +' width=200px height=210px > </p><p></p><h4 style=text-align:center>'+ \n
+                +' width=200px height=210px > </p><p></p><h4 style=\'height:50px; text-align:center\'>'+ \n
                 res.productos[indice]['nombre']+'</h4><p></p><p style=text-align:center> <button type=button  class= \'button1  btn btn-lg btn-success \' id='+ res.productos[indice]['idProducto'] +'>Ver más</button></p>');
                 }
             }
@@ -179,7 +179,7 @@ $('.feria').click(function(e){
                 var tamaño = Object.keys(res.productos).length;
                 for (var indice = 0; indice < tamaño; indice++) {
                 $( '.productos' ).append('<div class=\'col-lg-4 card \'><p style=text-align:center><img class=\'file-preview-image\' src='+direccion+'/aplicacion/CircuitoAgroturistico/web/uploads/'+ res.imagenes[indice] \n
-                +' width=200px height=210px > </p><p></p><h4 style=text-align:center>'+ \n
+                +' width=200px height=210px > </p><p></p><h4 style=\'height:50px; text-align:center\'+ \n
                 res.productos[indice]['nombre']+'</h4><p></p><p style=text-align:center> <button type=button  class= \'button1  btn btn-lg btn-success \' id='+ res.productos[indice]['idProducto'] +'>Ver más</button></p>');
                 }
             }
@@ -202,7 +202,7 @@ $('.localidad').click(function(e){
                 var tamaño = Object.keys(res.productos).length;
                 for (var indice = 0; indice < tamaño; indice++) {
                 $( '.productos' ).append('<div class=\'col-lg-4 card \'><p style=text-align:center><img class=\'file-preview-image\' src='+direccion+'/aplicacion/CircuitoAgroturistico/web/uploads/'+ res.imagenes[indice] \n
-                +' width=200px height=210px > </p><p></p><h4 style=text-align:center>'+ \n
+                +' width=200px height=210px > </p><p></p><h4 style=\'height:50px; text-align:center\'+ \n
                 res.productos[indice]['nombre']+'</h4><p></p><p style=text-align:center> <button type=button  class= \'button1  btn btn-lg btn-success \' id='+ res.productos[indice]['idProducto'] +'>Ver más</button></p>');
                 }
         })
