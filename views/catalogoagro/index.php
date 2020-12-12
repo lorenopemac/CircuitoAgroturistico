b996612baca6b217751fd23b8138843067f4287c
lorenopemac <lorenzopemac@gmail.com>
2020-12-12 20:57:17 -0300

    Funcionalidad faltante en agroturismo

--- views/catalogoagro/index.php
+++ views/catalogoagro/index.php
@@ -1,5 +1,5 @@
-    <?php
-
+<?php
+use yii\bootstrap\Modal;
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
@@ -7,21 +7,23 @@ use yii\helpers\Url;
 use kartik\select2\Select2;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
+use dosamigos\leaflet\types\LatLng;
+use dosamigos\leaflet\layers\Marker;
+use dosamigos\leaflet\layers\TileLayer;
+use dosamigos\leaflet\LeafLet;
+use dosamigos\leaflet\widgets\Map;  
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\ProductorSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
+\yii\web\YiiAsset::register($this);
 
 ?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+   integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+   crossorigin=""/>
 <style>
     .button1 {background-color: #4CAF50;} /* Green */
-    .en-linea {
-        display: flex;
-        justify-content: left;
-        align-items: left;
-        flex-wrap: wrap;
-        text-align: left;
-        border: 0px solid black;
-    }
     .categorias { float: initial; width:100%;}
     hr {
             border: 0;
@@ -34,69 +36,140 @@ use yii\web\NotFoundHttpException;
             margin-bottom: 1px;
         }
 
-        .producto {
-            border: 2px outset rgba(28,110,164,0.11);
-            border-radius: 6px;
-        }
-        .card {
-        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-        max-width: 300px;
-        margin: auto;
-        text-align: center;
-        font-family: arial;
-        margin-top: 10.5px;
-        }
+.producto {
+    border: 2px outset rgba(28,110,164,0.11);
+    border-radius: 6px;
+}
 
-        .price {
-        color: grey;
-        font-size: 22px;
-        }
+.modalButton{
+    width:100%;
+    height: 70px;
+}
 
-        .card button {
-        border: none;
-        outline: 0;
-        padding: 12px;
-        color: white;
-        background-color: #4CAF50;
-        text-align: center;
-        cursor: pointer;
-        width: 100%;
-        font-size: 18px;
-        }
+.cards {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 350px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+  margin-top: 10.5px;
+}
 
-        .card button:hover {
-        opacity: 0.7;
-        }
-        .file-preview-image{
-            height: 250px;
-            width: 100%;
-        }
+.price {
+  color: grey;
+  font-size: 22px;
+}
+
+.cards button {
+  border: none;
+  outline: 0;
+  padding: 12px;
+  color: white;
+  background-color: #4CAF50;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  font-size: 18px;
+}
+
+.cards button:hover {
+  opacity: 0.7;
+}
+.file-preview-image{
+    height: 300px;
+    width: 100%;
+}
+
+div.categorias {
+    overflow: auto;
+    max-height: 800px;
+}
+div.ferias {
+    overflow: auto;
+    max-height: 600px;
+}
+
+div.localidad{
+    overflow: auto;
+    max-height: 600px;
+}
+
+/* Style all font awesome icons */
+.fa {
+    padding: 10px;
+    font-size: 30px;
+    width: 50px;
+    height: 50px;
+    text-align: center;
+    text-decoration: none;
+    border-radius: 50%;
+    }
+
+    /* Add a hover effect if you want */
+    .fa:hover {
+    opacity: 0.7;
+    }
+
+    /* Set a specific color for each brand */
+
+    /* Facebook */
+    .fa-facebook {
+    background: #3B5998;
+    color: white;
+    }
+
+    /* Twitter */
+    .fa-twitter {
+    background: #55ACEE;
+    color: white;
+    }
+
+    .fa-instagram {
+    background: #E1306C;
+    color: white;
+    }
+
+    .fa-youtube {
+    background: #bb0000;
+    color: white;
+    }
+
 </style>
-    
-<div class="categoria-index" >
-<?php \yii\widgets\Pjax::begin();?>
+<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+   integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+   crossorigin=""></script>
+   <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs/dist/tf.min.js"> </script>
+   <script type="text/javascript">
+      // Notice how this gets configured before we load Font Awesome
+      window.FontAwesomeConfig = { autoReplaceSvg: false }
+    </script>
 
-<div  style=" box-sizing: border-box; display: flex; place-content: stretch ;">
+<div class="categoria-index" >
+
+
+<div   >
+    <div  style=" box-sizing: border-box; display: flex; place-content: stretch ;">
         <div  class="" id="filtros" style="flex: 1 1 100%; box-sizing: border-box; max-width: 20%; background: #d8dde6 ;"> 
             <div class="col-md-3"style=" width: 100%; padding-left:20%;">
                 <div  class="titulo"><h3><u>Categoría</u></h3></div>
                 <div class=" categorias">
+                    
                     <?php foreach($categorias as $categoria):?>        
-                        <label><input type="radio" class="categoria" name="categoria" id=<?= $categoria->idCategoria ?>> <?= $categoria->nombre ?> </label>
+                        <label><input type="radio" class=" categoria" name="categoria" id=<?= $categoria->idCategoria ?>> <?= $categoria->nombre ?> </label>
                         <hr>
                     <?php endforeach; ?>
                 </div>
                 <div  class="titulo"><h3><u>Feria</u></h3></div>
                 <div class="ferias">
                     <?php foreach($ferias as $feria):?>        
-                        <label><input type="radio" class="feria" name="feria" id=<?= $feria->idFeria ?>> <?= $feria->nombre ?> </label>
+                        <label><input type="radio" class="  feria" name="feria" id=<?= $feria->idFeria ?>> <?= $feria->nombre ?> </label>
                         <hr>
                     <?php endforeach; ?>
                 </div>
                 <div  class="titulo"><h3><u>Localidad</u></h3></div>
                 <div class="localidad">
                     <?php foreach($localidades as $localidad):?>        
-                        <label><input type="radio" class="localidad" name="localidad" id=<?= $localidad->idLocalidad ?>> <?= $localidad->nombre ?> </label>
+                        <label><input type="radio" class=" localidad" name="localidad" id=<?= $localidad->idLocalidad ?>> <?= $localidad->nombre ?> </label>
                         <hr>
                     <?php endforeach; ?>
                 </div>
@@ -106,20 +179,29 @@ use yii\web\NotFoundHttpException;
             
             <div class="productos">
                 <?php foreach($productos as $producto):?>
-                    <div class="col-lg-4 card">
+                    <div class="col-lg-4 cards">
                         <?= $producto->imagenes[0] ?>  
-
-                        <p></p><h4 style=" height:50px; text-align:center"><b> <?= $producto->nombre ?></b> </h4><p></p>
-
-                        <p style="text-align:center"><button type="button" class="btn btn-success" id= <?=$producto->idProducto?> >Ver más</button></p>
+                        <p></p>
+                        <b> <h4 style="height:50px; text-align:center"><?= $producto->nombre ?></h4></b> 
+                        <p style="height:10px"></p>
+                        <a class="modalButton btn btn-success" href="<?=Url::to(['producto/view', 'id'=>$producto->idProducto]); ?>"><h3>Ver más</h3></a>
                     </div>
                 <?php endforeach; ?>
+                <?php 
+                    Modal::begin([
+                        'header' => 'Ver Producto',
+                        'id' => 'modal',
+                        'size' => 'modal-lg',
+                    ]);
+                    echo "<div id='modalContent'></div>";
+                    Modal::end();
+                ?>
             </div>
         </div>
     </div>
 </div>
 
-<?php Pjax::end(); ?>
+
 
 </div>
 <?php
@@ -134,10 +216,12 @@ $this->registerJs(
 
 
 $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
+
 $urlFiltrarLocalidad = '../../aplicacion/CircuitoAgroturistico/web/catalogoagro/filtrolocalidad';
 $urlFiltrarCategoria = '../../aplicacion/CircuitoAgroturistico/web/catalogoagro/filtrocategoria';
 $urlFiltrarFeria = '../../aplicacion/CircuitoAgroturistico/web/catalogoagro/filtroferia';
-$urlProducto = Url::to(['producto/view']);
+
+$urlProducto = '../../aplicacion/CircuitoAgroturistico/web/producto/view';//Url::to(['producto/view']);
 $validar = false;
 $this->registerJs("
 
@@ -154,7 +238,7 @@ $('.categoria').click(function(e){
                 $( '.col-lg-4' ).remove();
                 var tamaño = Object.keys(res.productos).length;
                 for (var indice = 0; indice < tamaño; indice++) {
-                $( '.productos' ).append('<div class=\'col-lg-4 card \'><p style=text-align:center><img class=\'file-preview-image\' src='+direccion+'/aplicacion/CircuitoAgroturistico/web/uploads/'+ res.imagenes[indice] \n
+                $( '.productos' ).append('<div class=\'col-lg-4 cards \'><p style=text-align:center><img class=\'file-preview-image\' src='+direccion+'/aplicacion/CircuitoAgroturistico/web/uploads/'+ res.imagenes[indice] \n
                 +' width=200px height=210px > </p><p></p><h4 style= \'height:50px; text-align:center \'>'+  \n
                 res.productos[indice]['nombre']+'</h4><p></p><p style=text-align:center> <button type=button  class= \'button1  btn btn-lg btn-success \' id='+ res.productos[indice]['idProducto'] +'>Ver más</button></p>');
                 }
@@ -178,7 +262,7 @@ $('.feria').click(function(e){
                 $( '.col-lg-4' ).remove();
                 var tamaño = Object.keys(res.productos).length;
                 for (var indice = 0; indice < tamaño; indice++) {
-                $( '.productos' ).append('<div class=\'col-lg-4 card \'><p style=text-align:center><img class=\'file-preview-image\' src='+direccion+'/aplicacion/CircuitoAgroturistico/web/uploads/'+ res.imagenes[indice] \n
+                $( '.productos' ).append('<div class=\'col-lg-4 cards \'><p style=text-align:center><img class=\'file-preview-image\' src='+direccion+'/aplicacion/CircuitoAgroturistico/web/uploads/'+ res.imagenes[indice] \n
                 +' width=200px height=210px > </p><p></p><h4 style= \'height:50px; text-align:center \'>'+  \n
                 res.productos[indice]['nombre']+'</h4><p></p><p style=text-align:center> <button type=button  class= \'button1  btn btn-lg btn-success \' id='+ res.productos[indice]['idProducto'] +'>Ver más</button></p>');
                 }
@@ -201,7 +285,7 @@ $('.localidad').click(function(e){
                 $( '.col-lg-4' ).remove();
                 var tamaño = Object.keys(res.productos).length;
                 for (var indice = 0; indice < tamaño; indice++) {
-                $( '.productos' ).append('<div class=\'col-lg-4 card \'><p style=text-align:center><img class=\'file-preview-image\' src='+direccion+'/aplicacion/CircuitoAgroturistico/web/uploads/'+ res.imagenes[indice] \n
+                $( '.productos' ).append('<div class=\'col-lg-4 cards \'><p style=text-align:center><img class=\'file-preview-image\' src='+direccion+'/aplicacion/CircuitoAgroturistico/web/uploads/'+ res.imagenes[indice] \n
                 +' width=200px height=210px > </p><p></p><h4 style= \'height:50px; text-align:center \'>'+ \n
                 res.productos[indice]['nombre']+'</h4><p></p><p style=text-align:center> <button type=button  class= \'button1  btn btn-lg btn-success \' id='+ res.productos[indice]['idProducto'] +'>Ver más</button></p>');
                 }
@@ -209,6 +293,7 @@ $('.localidad').click(function(e){
         })
     }
 });
+
 
 $( '.productos' ).on('click','.btn', function(){
     console.log('$urlProducto');
@@ -219,5 +304,18 @@ $( '.productos' ).on('click','.btn', function(){
 
   });
 
+$('.modalButton').click(function (){
+    $.get($(this).attr('href'), function(data) {
+        $('#modal').modal('show').find('#modalContent').html(data);
+        
+
+    });
+    return false;
+});
+
+$('#modal').on('shown.bs.modal', function(event) {
+    window.dispatchEvent(new Event('resize'));
+});
+    
 ");
 ?>
