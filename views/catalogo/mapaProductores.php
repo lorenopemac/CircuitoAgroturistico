3d22c194ae80a45560255026cc5268c6e805f5e1
lorenopemac <lorenzopemac@gmail.com>
2020-12-14 21:15:23 -0300

    Fix solo se muestran productores con coordenas bien cargadas

--- views/catalogo/mapaProductores.php
+++ views/catalogo/mapaProductores.php
@@ -15,7 +15,7 @@ use dosamigos\leaflet\widgets\Map;
 // The Tile Layer (very important)
 $markers = [];
 foreach ($productores as $productor) {
-    if(!is_null($productor[1])){
+    if(!is_null($productor[1]) && !(strpos($productor[1],'°'))){
         $centerMarker = new LatLng(['lat' => $productor[1], 'lng' => $productor[2]]);
 
 // now lets create a marker that we are going to place on our map
